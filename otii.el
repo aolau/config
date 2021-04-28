48b3666abd77b0a5c9f317a1b64efdab00cdede0
Andreas Olausson <andreas.olausson@qoitech.com>
2021-04-28 16:43:24 +0200

    Fix build dir issue

--- otii.el
+++ otii.el
@@ -1,17 +1,20 @@
-(defvar *otii-core-root* (concat default-directory "/core"))
+(defvar *otii-core-root* default-directory)
 
 (defun otii-core-build-root ()
   (concat *otii-core-root* "/build"))
 
 (defun otii-core-build ()
   (interactive)
-  (and (cd (otii-core-build-root))
-       (compile "ninja")))
+  (compile (concat "cd " (otii-core-build-root) " && make -j8")))
 
 (defun otii-core-clean ()
   (interactive)
-  (and (cd (otii-core-build-root))
-       (shell-command "ninja clean")))
-  
+  (shell-command (concat "cd " (otii-core-build-root) " && make clean")))
+
+(defun otii-core-git-status ()
+  (interactive)
+  (magit-status *otii-core-root*))
+
 (global-set-key (kbd "C-c o b") 'otii-core-build)
 (global-set-key (kbd "C-c o c") 'otii-core-clean)
+(global-set-key (kbd "C-c o g") 'otii-core-git-status)
