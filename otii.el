9aef5b20e781357af423f569283ad78941debdff
Andreas Olausson <andreas.olausson@qoitech.com>
2024-11-11 11:33:49 +0100

    otii.el updates

--- otii.el
+++ otii.el
@@ -1,20 +1,45 @@
-(defvar *otii-core-root* default-directory)
+(defvar *otii-root* default-directory)
+(defvar *otii-root-core* (concat *otii-root* "/core"))
+(defvar *otii-root-core-build* (concat *otii-root-core* "/build"))
+(defvar *otii-root-desktop* (concat  *otii-root* "/client/desktop"))
+(defvar *otii-root-common* (concat  *otii-root* "/client/common"))
+(defvar *otii-root-server* (concat  *otii-root* "/client/server"))
+(defvar *otii-boost-root* "/home/andreas/code/boost_root")
 
-(defun otii-core-build-root ()
-  (concat *otii-core-root* "/build"))
-
-(defun otii-core-build ()
+(defun otii-build-core ()
   (interactive)
-  (compile (concat "cd " (otii-core-build-root) " && make -j8")))
+  (setenv "OTII_BOOST_ROOT" *otii-boost-root*)
+  (compile (concat "cd " *otii-root-core* " && cmake --build build --config RelWithDebInfo")))
 
-(defun otii-core-clean ()
+(defun otii-build-core-debug ()
   (interactive)
-  (shell-command (concat "cd " (otii-core-build-root) " && make clean")))
+  (setenv "OTII_BOOST_ROOT" *otii-boost-root*)
+  (compile (concat "OTII_BOOST_ROOT=" *otii-boost-root* " cd " *otii-root-core* " && cmake --build build --config Debug")))
 
-(defun otii-core-git-status ()
+(defun otii-build-desktop ()
   (interactive)
-  (magit-status *otii-core-root*))
+  (compile (concat "cd " *otii-root-desktop* " && tsc -b --pretty false")))
 
-(global-set-key (kbd "C-c o b") 'otii-core-build)
-(global-set-key (kbd "C-c o c") 'otii-core-clean)
-(global-set-key (kbd "C-c o g") 'otii-core-git-status)
+(defun otii-build-server ()
+  (interactive)
+  (compile (concat "cd " *otii-root-server* " && tsc -b --pretty false")))
+
+(defun otii-desktop-lint ()
+  (interactive)
+  (compile (concat "cd " *otii-root-desktop* " && npm run lint")))
+
+(defun otii-common-lint ()
+  (interactive)
+  (compile (concat "cd " *otii-root-common* " && npm run lint")))
+
+(defun otii-git-status ()
+  (interactive)
+  (magit-status *otii-root*))
+
+(global-set-key (kbd "C-c o b") 'otii-build-core)
+(global-set-key (kbd "C-c o B") 'otii-build-core-debug)
+
+(global-set-key (kbd "C-c o d") 'otii-build-desktop)
+(global-set-key (kbd "C-c o s") 'otii-build-server)
+
+(global-set-key (kbd "C-c o g") 'otii-git-status)
